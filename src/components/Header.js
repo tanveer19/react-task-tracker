6eac769b170cc7df89cb50abefcc97adcd515215
tanveer19 <thjbd19@gmail.com>
2021-05-30 08:23:08 +0600

    installed json server

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+import Button from "./Button";
+
+const Header = ({ title, onAdd, showAdd }) => {
+  
+
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button 
+      color={showAdd ? 'red' : 'green'}
+       text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+Header.prototype = {
+  title: PropTypes.string,
+};
+
+// css in js
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black',
+// }
+
+export default Header;
